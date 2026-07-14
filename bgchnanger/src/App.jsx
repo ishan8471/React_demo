42e4b4ae8a4ead8b55a43b4e165372a17473df73
Student Apna College <student@gmail.com>
2026-07-14 17:39:52 +0545

    bg-changer

---
+++ bgchnanger/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+
+
+function App() {
+  const [color, setColor] = useState('olive')
+
+  return (
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 
+      px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 
+        py-2 rounded-3xl  "> 
+        <button onClick={() => {setColor("red")}}
+        className="outline-none px-4 rounded-4xl text-white"
+        style={{backgroundColor:"red"}}>
+          Red
+        </button>
+        <button onClick={() => {setColor("green")}}
+        className="outline-none px-4 rounded-4xl text-white"
+        style={{backgroundColor:"green"}}>
+          Green
+        </button>
+        <button onClick={() => {setColor("blue")}}
+        className="outline-none px-4 rounded-4xl text-white"
+        style={{backgroundColor:"blue"}}>
+          Blue
+        </button>
+        <button onClick={() => {setColor("black")}}
+        className="outline-none px-4 rounded-4xl text-white"
+        style={{backgroundColor:"black"}}>
+          Black
+        </button>
+        <button onClick={() => {setColor("white")}}
+        className="border-1 px-4 rounded-4xl text-black"
+        style={{backgroundColor:"white"}}>
+          White
+        </button>
+        <button onClick={() => {setColor("skyblue")}}
+        className="outline-none px-4 rounded-4xl text-black"
+        style={{backgroundColor:"skyblue"}}>
+          Sky
+        </button>
+        <button onClick={() => {setColor("brown")}}
+        className="outline-none px-4 rounded-4xl text-white"
+        style={{backgroundColor:"brown"}}>
+          Brown
+        </button>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
